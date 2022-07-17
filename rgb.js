bc3356b5ffa8da10d5c4165d859bed1024bce6e3
lernercodes459 <lerners124@gmail.com>
2022-07-17 11:37:48 -0400

    I finished the rgb value hover feature now making the grid

--- rgb.js
+++ rgb.js
@@ -1,5 +1,34 @@
 const body = document.querySelector('body')
 
-body.addEventListener('onmouseover', (event) => {
-    event.target.textContent = 'nigga'
-})
+body.addEventListener('mouseover', rgbChange)
+
+function rgbChange(event){
+    let rgbStorage = ['rgb', '(', ')']
+
+    for(i = 0; i < 3; i++){
+
+        const num = randomInt(255).toString()
+        if(i != 2){
+            rgbStorage.splice((2 * i) + 2, 0, num, ',')
+        }else{
+            rgbStorage.splice((2 * i) + 2, 0, num)
+        }
+        
+    }
+    const value = rgbStorage.join('')
+    
+    event.target.style.color = value
+}
+
+function randomInt(max){
+    return Math.floor(Math.random() * max)
+}
+
+function makeGrid(){
+    makeRows()
+    makeCells()
+}
+
+function makeRows(amount){
+    
+}
